b593dff4d5a32ba2e172371f41d745b85aacb697
laramie17goode@gmail.com <laramie17goode@gmail.com@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-05 03:25:38 +0000

    Some framework/skeleton code. Need help/clarification with the Domain and Value classes. Hoping to have only 1 spot in memory for each cell and have everything that goes to it be pointers but we'll see.

---
+++ trunk/DatabaseEngine.cpp
@@ -0,0 +1,131 @@
+/******************************** 
+ * Laramie Goode				*
+ * CSCE-315-Project_1			*
+ * Database Engine (WIP)		*
+ ********************************/
+
+#include "stdlib.h" 
+#include <string>
+#include <map>
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Attribute {
+
+	string name;
+	vector<string> cells;
+	
+public:
+
+	Attribute(string input_name) {
+		name = input_name;
+	}
+	
+	void addCell(string value) {
+		cells.push_back(value);
+	}
+	
+	int getCellIndex(string value) {
+	
+		int i = 0;
+		
+		while( strcmp(cells[i].c_str(), value.c_str()) != 0)  {
+			i++;
+		}
+		
+		if(i < cells.size()) {
+			return i;
+		} else {
+			return -1;
+			//error value, watch out for vector[-1] results
+		}
+	}
+	
+	void setCell(string old_value, string new_value) {
+		cells[findCell(old_value)] = new_value;
+	}
+	
+};
+
+class Tuple {
+	
+	vector<string*> cells;
+	//Gonna try and have these cells point to the ones inside the attributes vector
+	//Should cause updates/changes to be smoother
+};
+
+class Relation {
+
+	string name;
+	map<string, Attribute> columns;
+	vector<Tuple> rows;
+	
+public:
+
+	Relation(string input_name) {
+		name = input_name;
+	}
+	
+	void addAttribute(string name) {
+	
+		Attribute attr(name);
+		
+		columns.insert( pair<string,Attribute>(name, attr) );	
+	
+	}
+	
+	void addTuple(Tuple row) {
+		
+	}
+
+	string getName() {
+		return name;
+	}
+	
+	string stringify() {
+	
+		return "0";
+		//Idea behind this is to convert the relation into a string
+		//The string can then be written to a file
+	}
+	
+
+};
+
+class Domain {
+	//halp
+};
+
+class Value {
+	//halp
+};
+
+template <class DB_type>
+void writeToFile(DB_type writeFrom) {
+
+	ofstream outputFile;
+	outputFile.open(writeFrom.getName() + ".db");
+	outputFile << writeFrom.stringify();
+	outputFile.close();
+	
+}
+
+void readFromFile(string input) {
+
+	ifstream inputFile;
+	inputFile.open(input.c_str());
+	
+	//copy relation from .db file to memory (incomplete)
+	Relation importedRelation(input);
+	
+	inputFile.close();
+	
+}
+
+int main() {
+
+	return 0;
+}
