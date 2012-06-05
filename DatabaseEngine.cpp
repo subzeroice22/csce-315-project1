bf374df655d2a9ebb17f758b0d5f0bfbad36387c
laramie17goode@gmail.com <laramie17goode@gmail.com@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-05 19:53:52 +0000

    Gave Tuple the ability to point to the specific cells in the right attribute. Implementing add and delete operations for Tuple/Attribute.

--- DatabaseEngine.cpp
+++ DatabaseEngine.cpp
@@ -24,6 +24,10 @@ public:
 		name = input_name;
 	}
 	
+	string* getName() {
+		return &name;
+	}
+	
 	void addCell(string value) {
 		cells.push_back(value);
 	}
@@ -45,16 +49,42 @@ public:
 	}
 	
 	void setCell(string old_value, string new_value) {
-		cells[findCell(old_value)] = new_value;
+		cells[getCellIndex(old_value)] = new_value;
 	}
 	
 };
 
 class Tuple {
 	
-	vector<string*> cells;
+	//vector<string*> cells;
 	//Gonna try and have these cells point to the ones inside the attributes vector
 	//Should cause updates/changes to be smoother
+	
+public:
+
+	vector<string*> cells;
+
+	void add(Attribute* attr) {
+		cells.push_back( attr->getName() );
+	}
+	
+	int getCellIndex(string value) {
+		//copy-pasted from attribute, might just inherit these from the same place
+	
+		int i = 0;
+				
+		while( strcmp(((string*)cells[i])->c_str(), value.c_str()) != 0 && i < cells.size())  {
+			i++;
+		}
+	
+		if(i < cells.size()) {
+			return i;
+		} else {
+			return -1;
+			//error value, watch out for vector[-1] results
+		}
+	}
+	
 };
 
 class Relation {
@@ -72,8 +102,21 @@ public:
 	void addAttribute(string name) {
 	
 		Attribute attr(name);
+		columns.insert( pair<string,Attribute>(name, attr) );
 		
-		columns.insert( pair<string,Attribute>(name, attr) );	
+		//columns[name]
+	
+	}
+	
+	void deleteAttribute(string name) {
+	
+		//deletes the cell pointer in every row inside the row vector
+		for(int i = 0; i < rows.size(); i++) {
+			rows[i].cells.erase(rows[i].cells.begin()+rows[i].getCellIndex(name));
+		}
+	
+		//deltes the named column
+		columns.erase(name);
 	
 	}
 	
