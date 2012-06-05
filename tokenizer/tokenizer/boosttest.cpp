cf7318419a28dc57395fda924708bdaeb4bcd9c0
curtisfranks <curtisfranks@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-05 21:28:16 +0000

    Added code to take the test input from a text file to a string "inputFile" so that tokenizer could utilize "inputFile"

--- tokenizer/tokenizer/boosttest.cpp
+++ tokenizer/tokenizer/boosttest.cpp
@@ -1,17 +1,29 @@
 // simple_example_4.cpp
 #include<iostream>
+#include<fstream>
 #include<boost/tokenizer.hpp>
 #include<string>
+
+
+
 
 int main(){
    using namespace std;
    using namespace boost;
    
-   string s = "This (is),  a + test";
+   ifstream inputFile("testinput.txt");
    string end;
+   string content((istreambuf_iterator<char>(inputFile)),
+				(istreambuf_iterator<char>()));
    char_separator<char> separator( "", "()+-" );
-   tokenizer<char_separator<char> > tok(s);
-   for(tokenizer<char_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg){
+
+
+
+   
+
+   tokenizer<char_separator<char> > tok(content);
+   for(tokenizer<char_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg)
+   {
        cout << *beg << "\n";
    }
    
