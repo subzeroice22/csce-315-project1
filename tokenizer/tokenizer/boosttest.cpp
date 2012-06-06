6db5a0d687ba8b6ee2a0b0bf4dd54b853ca66884
curtisfranks <curtisfranks@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-06 00:16:45 +0000

    determined method so that Tokenizer recognizes consecutive symbols as one token.

--- tokenizer/tokenizer/boosttest.cpp
+++ tokenizer/tokenizer/boosttest.cpp
@@ -9,7 +9,8 @@ int main(){
    using namespace boost;
    
    ifstream inputFile("testinput.txt");
-   string end;
+   string end,temp="";
+   bool openToken=false;
    string content((istreambuf_iterator<char>(inputFile)),
 				(istreambuf_iterator<char>()));
    char_separator<char> separator(" \n","\"()+<>=-;");
@@ -18,12 +19,35 @@ int main(){
    tokenizer<char_separator<char> > tok(content, separator);
    for(tokenizer<char_separator<char> >::iterator beg=tok.begin(); beg!=tok.end();++beg)
    {
-
-		   tokens.push_back(*beg);
-	   cout << tokens.back() << "\n";
+	   if(openToken==true)
+	   {
+			if(*beg=="<"||*beg==">"||*beg=="="||*beg=="\""||*beg=="("||*beg==")"||*beg=="-")
+			{
+				openToken=true;
+				temp=temp+*beg;cout<<"saw "<<*beg<<" so openToken stays true and temp set to "<<temp<<endl;
+			}
+			else
+			{
+				tokens.push_back(temp);cout<<"Token   "<<temp<<endl;
+				tokens.push_back(*beg);cout<<"Token   "<<*beg<<endl;
+				temp="";
+				openToken=false;
+			}
+	   }
+	   else if(openToken==false)
+	   {
+			if(*beg=="<"||*beg==">"||*beg=="="||*beg=="\""||*beg=="("||*beg==")"||*beg=="-")
+			{
+				openToken=true;
+				temp=*beg;cout<<"openToken was false and saw "<<*beg<<" so openToken set to true and temp set to "<<*beg<<endl;
+			}
+			else
+			{
+				tokens.push_back(*beg);cout<<"Token   "<<*beg<<endl;
+				openToken=false;cout<<"saw "<<*beg<<" so openToken set to false and pushed "<<*beg<<endl;
+			}
+	   }
    }
-   
-   
 	cout << "Press ENTER to quit";
 	cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
