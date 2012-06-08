2952ba482008698f8475007b27135e4220d6bdbc
ckvoss@gmail.com <ckvoss@gmail.com@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-08 20:15:03 +0000

    small code readability change

--- trunk/parser.cpp
+++ trunk/parser.cpp
@@ -11,147 +11,6 @@
 using namespace std;
 
 
-/* token consumption information
-"primitive" functions which consume all of their input:
-isLiteral();
-isIdentifier();
-isInteger();
-
-Additionally all terminal/special tokens (i.e. '(' , '||' , ',' , 'SELECT' , etc ) will be comsumed in the highest level non-terminal function in which they appear 
-
-//isType() is a good example. it must consume all tokens EXCEPT for <integer> if it is a VARCHAR
-*/
-
-vector<string> dbTokens(string commandLine){
-//FUNCTION DECLARATIONS  
-	string first;
-	string temp="";			//the temp string is needed in order to store tokens produced by tokenizer when it may
-							//be necessary to concatenate them with adjoining tokens to keep appropriate values within
-							//the the command together.
-	bool openSymbol=false,closeSymbol=false,openQuote=false;	//these boolean variable are utilized to keep track of symbols
-																//that may have multiple parts and to ensure that text within 
-																//quotations is held together as one token, regardless of 
-																//punctuation or spaces.
-	boost::char_separator<char> separator(" \n","\"()+<>=-;,!");	//the tokenizer function allows for the declaration of ignored and
-																	//returned symbols with the ignored before the comma
-	vector<string> tokens;	//a vector of type string to store the tokens so they may be returned to the calling program
-	boost::tokenizer< boost::char_separator<char> > possibleTokens(commandLine, separator);	//the boost library supplies this function
-	//END OF FUNCTION DECLARATIONS
-   
-	for( boost::tokenizer< boost::char_separator<char> >::iterator position=possibleTokens.begin();
-		position!=possibleTokens.end();
-		++position){
-	   if(openQuote==true)							//With each pass through this for loop, the position is iterated
-	   {											//If statements are used to check each token against possible 
-			if(*position=="\"")						//conjunctive symbols such as <=, ==, >==, etc.
-			{										//The first priority of the conditional checking is for an open quotation.
-				tokens.push_back(temp);				//If the passed command has quotations around two otherwise separated words,
-				tokens.push_back(*position);		//such as "two words", the openQuote boolean flag will stay true until the
-				temp="";							//quote is finished and the strings within are concatenated into a token.
-				openSymbol=false;
-				closeSymbol=false;
-				openQuote=false;
-			}
-			else
-				if(temp=="")
-					temp=*position;
-				else
-				{
-					first=*position;
-					if(!isalpha(first[0]))
-						temp=temp+*position;		//if the current string is a symbol and still within the quotation then
-					else							//no space will be added.
-						temp=temp+" "+*position;	//the temp string is storing the concatenated strings within the quotes,
-				}									//using a space between if both strings are text.
-	   }
-	   else if(openSymbol==true)					//There are four types of data that this chain handles: openSybols, 
-	   {											//closeSymbols, quotations, and strings that would be data or commands.
-			if(*position=="\"")						//When the openSymbol was previously set to true, the current position
-			{										//is then checked for another openSymbol that may need to be joined with the
-				tokens.push_back(temp);				//previous or if the current position requires that a new flag be set to true.
-				tokens.push_back(*position);
-				temp="";
-				openSymbol=false;
-				closeSymbol=false;
-				openQuote=true;						//In this case a quotation is found and determined to be an opening quote.
-			}
-			else if(*position=="<"||*position=="="||*position=="-")
-			{
-				openSymbol=true;					//In this case an adjoining openSymbol is found and concatenated to the previous.
-				temp=temp+*position;				//This conditional check handles <=, ==, and <- symbols.
-			}
-			else
-			{
-				tokens.push_back(temp);
-				tokens.push_back(*position);
-				temp="";
-				openSymbol=false;
-				closeSymbol=false;					//when no symbols are found in this case, the previous open symbol is tokenized
-			}										//and the current string is tokenized as well.
-	   }
-	   else if(closeSymbol==true)
-	   {
-		   if(*position=="\"")
-			{
-				tokens.push_back(*position);
-				temp="";
-				openSymbol=false;
-				closeSymbol=false;		
-				openQuote=true;						//When the prior string was a closeSymbol and a quotation is found it can be
-			}										//assumed this is an open quotation.
-		   else if(*position==">"||*position=="=")
-			{
-				closeSymbol=true;					//Here an adjoining closeSymbol is found and concatenated to the previous symbol.
-				temp=temp+*position;				//This handles the >= symbol.
-			}
-		   else if(*position=="-")
-			{
-				closeSymbol=false;					//If the previous symbol was a closeSymbol then <- will be accounted for.
-				openSymbol=true;
-				tokens.push_back(temp);
-				temp=*position;
-
-			}
-		   else
-			{
-				tokens.push_back(temp);
-				tokens.push_back(*position);		//Lastely, when the previous string was a closeSymbol and the current string is not 
-				closeSymbol=false;					//a symbol, the previous symbol will be tokenized and the current string will be tokenized.
-				temp="";
-			}
-	   }
-	   else
-	   {
-		   if(*position=="\"")
-		   {
-				tokens.push_back(*position);
-				temp="";
-				openSymbol=false;
-				closeSymbol=false;
-				openQuote=true;						//If the previous string was not a symbol and there was no previous openQuote, this quote
-		   }										//will be treated as an opening quote.
-		   else if(*position=="<"||*position=="="||*position=="-"||*position=="!")		//As with the previous conditional checks, this check
-			{																			//accounts for all possible openSymbols.
-				openSymbol=true;
-				temp=temp+*position;
-			}
-		   else if(*position==">")
-			{
-				closeSymbol=true;
-				temp=*position;
-			}
-		   else
-		   {
-				closeSymbol=false;
-				openSymbol=false;
-				tokens.push_back(*position);		//All other strings are tokenized.
-				temp="";
-		   }
-	   }
-   }
-   return tokens;			//A vector full of the tokens ready for translation by the database engine.
-}
-
 class Parser{
 public:
 	bool allowNonCaps; //True = allow mixed-case Command keywords (i.e. SELECT vs sElecT)
@@ -226,6 +85,16 @@ private:
 	vector<string> sToks;
 	int sI;
 	int level;
+	/* token consumption information
+	"primitive" functions which consume all of their input:
+	isLiteral();
+	isIdentifier();
+	isInteger();
+
+	Additionally all terminal/special tokens (i.e. '(' , '||' , ',' , 'SELECT' , etc ) will be comsumed in the highest level non-terminal function in which they appear 
+
+	//isType() is a good example. it must consume all tokens EXCEPT for <integer> if it is a VARCHAR
+	*/
 
 	void resetParserVals(){
 		sToks.clear();
@@ -245,6 +114,137 @@ private:
 		}
 		return upp;
 }
+
+	//Scanner
+	vector<string> dbTokens(string commandLine){
+	//FUNCTION DECLARATIONS  
+		string first;
+		string temp="";			//the temp string is needed in order to store tokens produced by tokenizer when it may
+								//be necessary to concatenate them with adjoining tokens to keep appropriate values within
+								//the the command together.
+		bool openSymbol=false,closeSymbol=false,openQuote=false;	//these boolean variable are utilized to keep track of symbols
+																	//that may have multiple parts and to ensure that text within 
+																	//quotations is held together as one token, regardless of 
+																	//punctuation or spaces.
+		boost::char_separator<char> separator(" \n","\"()+<>=-;,!");	//the tokenizer function allows for the declaration of ignored and
+																		//returned symbols with the ignored before the comma
+		vector<string> tokens;	//a vector of type string to store the tokens so they may be returned to the calling program
+		boost::tokenizer< boost::char_separator<char> > possibleTokens(commandLine, separator);	//the boost library supplies this function
+		//END OF FUNCTION DECLARATIONS
+	   
+		for( boost::tokenizer< boost::char_separator<char> >::iterator position=possibleTokens.begin();
+			position!=possibleTokens.end();
+			++position){
+		   if(openQuote==true)							//With each pass through this for loop, the position is iterated
+		   {											//If statements are used to check each token against possible 
+				if(*position=="\"")						//conjunctive symbols such as <=, ==, >==, etc.
+				{										//The first priority of the conditional checking is for an open quotation.
+					tokens.push_back(temp);				//If the passed command has quotations around two otherwise separated words,
+					tokens.push_back(*position);		//such as "two words", the openQuote boolean flag will stay true until the
+					temp="";							//quote is finished and the strings within are concatenated into a token.
+					openSymbol=false;
+					closeSymbol=false;
+					openQuote=false;
+				}
+				else
+					if(temp=="")
+						temp=*position;
+					else
+					{
+						first=*position;
+						if(!isalpha(first[0]))
+							temp=temp+*position;		//if the current string is a symbol and still within the quotation then
+						else							//no space will be added.
+							temp=temp+" "+*position;	//the temp string is storing the concatenated strings within the quotes,
+					}									//using a space between if both strings are text.
+		   }
+		   else if(openSymbol==true)					//There are four types of data that this chain handles: openSybols, 
+		   {											//closeSymbols, quotations, and strings that would be data or commands.
+				if(*position=="\"")						//When the openSymbol was previously set to true, the current position
+				{										//is then checked for another openSymbol that may need to be joined with the
+					tokens.push_back(temp);				//previous or if the current position requires that a new flag be set to true.
+					tokens.push_back(*position);
+					temp="";
+					openSymbol=false;
+					closeSymbol=false;
+					openQuote=true;						//In this case a quotation is found and determined to be an opening quote.
+				}
+				else if(*position=="<"||*position=="="||*position=="-")
+				{
+					openSymbol=true;					//In this case an adjoining openSymbol is found and concatenated to the previous.
+					temp=temp+*position;				//This conditional check handles <=, ==, and <- symbols.
+				}
+				else
+				{
+					tokens.push_back(temp);
+					tokens.push_back(*position);
+					temp="";
+					openSymbol=false;
+					closeSymbol=false;					//when no symbols are found in this case, the previous open symbol is tokenized
+				}										//and the current string is tokenized as well.
+		   }
+		   else if(closeSymbol==true)
+		   {
+			   if(*position=="\"")
+				{
+					tokens.push_back(*position);
+					temp="";
+					openSymbol=false;
+					closeSymbol=false;		
+					openQuote=true;						//When the prior string was a closeSymbol and a quotation is found it can be
+				}										//assumed this is an open quotation.
+			   else if(*position==">"||*position=="=")
+				{
+					closeSymbol=true;					//Here an adjoining closeSymbol is found and concatenated to the previous symbol.
+					temp=temp+*position;				//This handles the >= symbol.
+				}
+			   else if(*position=="-")
+				{
+					closeSymbol=false;					//If the previous symbol was a closeSymbol then <- will be accounted for.
+					openSymbol=true;
+					tokens.push_back(temp);
+					temp=*position;
+
+				}
+			   else
+				{
+					tokens.push_back(temp);
+					tokens.push_back(*position);		//Lastely, when the previous string was a closeSymbol and the current string is not 
+					closeSymbol=false;					//a symbol, the previous symbol will be tokenized and the current string will be tokenized.
+					temp="";
+				}
+		   }
+		   else
+		   {
+			   if(*position=="\"")
+			   {
+					tokens.push_back(*position);
+					temp="";
+					openSymbol=false;
+					closeSymbol=false;
+					openQuote=true;						//If the previous string was not a symbol and there was no previous openQuote, this quote
+			   }										//will be treated as an opening quote.
+			   else if(*position=="<"||*position=="="||*position=="-"||*position=="!")		//As with the previous conditional checks, this check
+				{																			//accounts for all possible openSymbols.
+					openSymbol=true;
+					temp=temp+*position;
+				}
+			   else if(*position==">")
+				{
+					closeSymbol=true;
+					temp=*position;
+				}
+			   else
+			   {
+					closeSymbol=false;
+					openSymbol=false;
+					tokens.push_back(*position);		//All other strings are tokenized.
+					temp="";
+			   }
+		   }
+	   }
+	   return tokens;			//A vector full of the tokens ready for translation by the database engine.
+	}
 
 	bool ParseSingleLine(string line){
 		resetParserVals();
@@ -1027,4 +1027,8 @@ int main(){
 	cout<<"Exiting...\n";
 	return 0;
 }
+
+
+
+
 
