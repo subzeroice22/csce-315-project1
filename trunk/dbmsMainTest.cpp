e3800a5cea1347293633bbb70398bd5b9c4e375f
ckvoss@gmail.com <ckvoss@gmail.com@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-11 20:57:52 +0000

    added dbmsMainTest.cpp for testing DBMS.h removed DBMS.cpp

---
+++ trunk/dbmsMainTest.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>//|
+#include "DBMS.h"
+
+using namespace std;
+
+int main(){
+/* DVD_APP USAGE EXAMPLE (NOT TESTED) (SUBJECT TO CHANGE)
+	string sampleCommand1 = "CREATE TABLE baseball_players (fname VARCHAR(20), lname VARCHAR(30), team VARCHAR(20), homeruns INTEGER, salary INTEGER) PRIMARY KEY (fname, lname);";
+	string sampleCommand2 = "INSERT INTO baseball_players VALUES FROM (\"Joe\", \"Swatter\", \"Pirates\", 40, 1000000);";
+	string sampleCommand3 = "INSERT INTO baseball_players VALUES FROM (\"Sarah\", \"Batter\", \"Dinosaurs\", 100, 5000000);";
+	string sampleCommand4 = "INSERT INTO baseball_players VALUES FROM (\"Snoopy\", \"Slinger\", \"Pirates\", 3, 200000);";
+	string sampleQuery1 = "high_hitters <- select (homeruns >= 40) baseball_players;"
+	string sampleCommand5 = "SHOW high_hitters";
+	
+	DBMS exDBMS1(false, 0, "./DVDDataBase"); //use this constructor for DVD_APP
+	exeDBMS1.Execute(SampleCommand1);
+	string Program = sampleCommand2 + sampleCommand3 + sampleCommand4;
+	exeDBMS1.Excute(Program);
+	exeDBMS1.Excute(sampleQuery1);
+	exeDBMS1.Excute(sampleCommand5);
+*/
+	cout<<"ENTERING MAIN!!!\n\n\n";
+	DBMS* dbms1 = new DBMS();
+	delete dbms1;
+	cout<<"LEAVING MAIN!!!\n\n\n";	
+		
+	return 0;
+}
+
