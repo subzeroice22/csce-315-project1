10edba96c20cde50ead960e31aca25db2b57eaf9
ckvoss@gmail.com <ckvoss@gmail.com@3b675838-1f10-7977-cf85-3f854b0d20ca>
2012-06-13 01:11:24 +0000

    Added select functionality and full conditional functionality

--- CondConjCompOp.h
+++ CondConjCompOp.h
@@ -2,8 +2,9 @@
 #define CONDCONJCOMPOP_H
 
 #include "Relation.h"
+#include "Helpers.h"
 
-enum Operation { Equality=0, NonEquality, LessThan, GreaterThan, LessThanEqual, GreaterThanEqual }; // ==, !=, <, >, <=, >=
+enum Operation { Equality=0, NonEquality, LessThanEqual, GreaterThanEqual, LessThan, GreaterThan}; // ==, !=, <, >, <=, >=
 
 class Operand{
 public:
@@ -61,9 +62,13 @@ bool Comparison::passes(Relation* relation, int tupleIndex){
 		return cond.passes(relation, tupleIndex);
 	}
 	else{
+		bool intFlag;
+		int ival1, ival2;
 		string val1, val2;
 		if(operand1.isAttribute){
+			intFlag = relation->findAttribute(operand1.val).type.isInt();
 			val1=relation->findAttribute(operand1.val).cells[tupleIndex];
+			if(intFlag) ival1=Helpers::stringToInt(val1);
 		}else{
 			val1=operand1.val;
 		}
@@ -71,26 +76,50 @@ bool Comparison::passes(Relation* relation, int tupleIndex){
 			val2=relation->findAttribute(operand2.val).cells[tupleIndex];
 		}else{
 			val2=operand2.val;
+			if(intFlag) ival2=Helpers::stringToInt(val2);
 		}
-		switch (op){
-			case 0:
-				return val1==val2;
-				break;
-			case 1:
-				return val1!=val2;
-				break;
-			case 2:
-				return val1<=val2;
-				break;
-			case 3:
-				return val1>=val2;
-				break;
-			case 4:
-				return val1<val2;
-				break;
-			case 5:
-				return val1>val2;
-				break;
+		if(!intFlag){
+			switch (op){
+				case 0:
+					return val1==val2;
+					break;
+				case 1:
+					return val1!=val2;
+					break;
+				case 2:
+					return val1<=val2;
+					break;
+				case 3:
+					return val1>=val2;
+					break;
+				case 4:
+					return val1<val2;
+					break;
+				case 5:
+					return val1>val2;
+					break;
+			}
+		}else{
+			switch (op){
+				case 0:
+					return ival1==ival2;
+					break;
+				case 1:
+					return ival1!=ival2;
+					break;
+				case 2:
+					return ival1<=ival2;
+					break;
+				case 3:
+					return ival1>=ival2;
+					break;
+				case 4:
+					return ival1<ival2;
+					break;
+				case 5:
+					return ival1>ival2;
+					break;
+			}
 		}
 	}
 }
